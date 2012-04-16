5d0648eaa638547c12ee4428637295e7af555ae8
Nourhen <nourhen1991@hotmail.com>
2012-04-16 15:19:49 +0300

    first commit

---
+++ Desktop/Nunu/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nunu::Application.config.secret_token = '6609598888e9eec5aa813ebf80880463357ac94e9b8dbd8140bb4463797025529d847f54aa9c78de904fdb9f39772bfeac463351e0e9c790be7c83bfcaae4260'
